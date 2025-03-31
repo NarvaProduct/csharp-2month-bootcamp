a6ea15d49edc656d8c0ec834a8d9764870776e11
Tom <explorerman951@gmail.com>
2025-03-31 17:49:07 -0400

    Added GetPlayerStats and DisplayPlayerStats

--- CsharpBootCamp/Program.cs
+++ CsharpBootCamp/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, my name a balsemic vinnigrette!");
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            string name; int age; string classType; 
+            GetPlayerStats(out name, out age, out classType);
+            DisplayPlayerStats(name, age, classType);
+        }
+        public static void GetPlayerStats(out string name, out int age, out string classType)
+        {
+            Console.WriteLine("What is your player's name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What is your player's age?");
+            string stringAge = Console.ReadLine();
+            while (!int.TryParse(stringAge, out age))
+            {
+                Console.WriteLine("Please enter an integer for the player's age.");
+                stringAge = Console.ReadLine();
+            }
+            
+            Console.WriteLine("What is your player's class?");
+            classType = Console.ReadLine();
+        }
+        public static void DisplayPlayerStats(string name, int age, string classType)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Player Profile ===");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Class: {classType}");
+        }
+    }
+}
